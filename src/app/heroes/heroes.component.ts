655e6e51771d7486fb935089f6a6aaab6b4c4411
Yuuki-Imagawa0827 <yuuki.imagawa0827@gmail.com>
2023-09-12 12:03:43 +0900

    サービスクラス実装

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES }  from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +9,18 @@ import { HEROES }  from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
- heroes=HEROES;
+ heroes: Hero[] = [];
  selectedHero?: Hero;
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
+}
+constructor(private heroService: HeroService) {}
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
+ngOnInit(): void {
+  this.getHeroes();
 }
 
 }
