050d40696a612c1e0d84c3ae9d9a949379766f31
Yuuki-Imagawa0827 <yuuki.imagawa0827@gmail.com>
2023-09-12 22:29:04 +0900

    サービスクラス追加

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import {MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor() { }
+  //HeroサービスでmessageServiceを使用する
+  constructor(private messageService:MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    //HEROESをObservableオブジェクトに変換
     const heroes = of(HEROES);
+    //messageServiceに文言追加
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
